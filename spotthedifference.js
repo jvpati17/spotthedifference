7d61ad37bc9b54d1e84a24606dd38a3519cad313
jvpati17 <139751449+jvpati17@users.noreply.github.com>
2023-08-10 07:05:04 -0700

    no.comment

--- spotthedifference.js
+++ spotthedifference.js
@@ -1,25 +1,16 @@
 const gameTitle = 'Spot the Difference';
 
 const startButton = document.getElementById('start-button').style.display = 'none';
-    
-//startButton.addEventListener('click', function(){
-    //showBoard();
-    //console.log('hi')
-//});
-    //startGame();
 let playerScore = document.getElementById('player-score').style.display = 'none';
 let board = document.getElementById('board').style.display = 'none';
 let board2 = document.getElementById('boardTwo').style.display = 'none';
 let board3 = document.getElementById('boardThree').style.display = 'none';
 const tryAgain = document.getElementById('try-again').style.display = 'none';
 
-//let winningChoice = false;
+
 
 let roundTwoButton = document.getElementById('round-2').style.display = 'none';
 let roundThreeButton = document.getElementById('round-3').style.display = 'none';
-//function roundTwo() {
-    //document.getElementById('round-2').style.display = 'grid';
-//}
 
 function secretStart() {
     document.getElementById('start-button').style.display = 'inline';
@@ -27,18 +18,7 @@ function secretStart() {
 
 
 
-// function startGame() {
-    //console.log('hello')
-//}
 
-//function showBoard() {
-    //document.getElementById('board').style.display = 'grid'; 
-    //document.getElementById('try-again').style.display = 'none';
-    
-      
-    //}
-    
-    //console.log('hi')
 
     function hideBoard() {
         document.getElementById('board').style.display = 'grid';
@@ -50,7 +30,6 @@ function secretStart() {
     function changeColor() {
         document.getElementById('winning-cell').innerHTML = 'Correct!';
         document.getElementById('winning-cell').style.backgroundColor = 'white';
-        //document.getElementById('try-again').style.display = 'grid';
         document.getElementById('board').style.backgroundColor = 'white';
         document.getElementById('round-2').style.display = 'inline';
         document.getElementById('start-button').style.display = 'none';
@@ -113,108 +92,3 @@ function secretStart() {
         
         }
         
-
-
-
-/* window.onload = function() {
-    setGame();
-}
-function setGame() {
-    board = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
-    ]
-
-    for (let r = 0; r < 3; r++){
-        for (let c = 0; c < 3; c++) {
-            let cell = document.createElement('div');
-            cell.id = r.toString() + '-' + c.toString();
-            cell.classList.add('cell');
-            if (r == 0 || r == 1) {
-                cell.classList.add('horizontal-line');
-            }
-            if (c == 0 || c == 1) {
-                cell.classList.add('vertical-line');
-            }
-            cell.addEventListener('click', checkChoice);
-            document.getElementById('board').append(cell);
-        }
-    }
-}
-
-function checkChoice() {
-    if (winningChoice) {
-        return;
-    }
-
-    let coords = this.id.split('-')
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
-
-    if (board[r][c] != winningChoice) {
-        return;
-    }
-
-}
-*/
-
-
-
-
-
-
-
-
-
-//board hide function
-
-//function hideUnhide(id) {
-    //if (board.style.display == 'none') {
-        //board.style.display = 'block';
-    //} else {
-       // board.style.display = 'none';
-    //}
-//}
-
-
-
-
-
-
-
-
-
-
-//startButton.addEventListener('click', firstRound);
-//function firstRound() {
-   // if ()
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const board = document.getElementById('board');
-//board.addEventListener('click', function(){
-    //console.log('hi')
-//});
-
-//const startButton = document.getElementById('start-button');
-//startButton.addEventListener('click', firstRound);
-
-//function firstRound()
