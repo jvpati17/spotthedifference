9a49658536f941551e0c2917ccb7847c2ffcaaa8
jvpati17 <139751449+jvpati17@users.noreply.github.com>
2023-08-10 16:38:42 -0700

    no.on.click

--- spotthedifference.js
+++ spotthedifference.js
@@ -34,6 +34,8 @@ const roundThreeButton = document.getElementById('round-3').style.display = 'non
 //})
 
 document.getElementById('board').addEventListener('click', handleClick)
+document.getElementById('board2').addEventListener('click', handleClick)
+document.getElementById('board3').addEventListener('click', handleClick)
 
 /*----- (button) eventlisteners -----*/
 
@@ -45,8 +47,8 @@ document.getElementById('round-3').addEventListener('click', boardThree)
 /*----- (winning cell) eventlisteners -----*/
 
 //document.getElementById('winning-cell').addEventListener('click', changeColor)
-document.getElementById('winning-cell-2').addEventListener('click', winningCell2)
-document.getElementById('winning-cell-3').addEventListener('click', winningCell3)
+//document.getElementById('winning-cell-2').addEventListener('click', winningCell2)
+//document.getElementById('winning-cell-3').addEventListener('click', winningCell3)
 
 //document.querySelectorAll('cell').addEventListener('click', wrongCell)
 
@@ -58,6 +60,24 @@ function handleClick(evt) {
         changeColor()
     } else if (evt.target.className === 'cell') {
         wrongCell()
+    }
+
+}
+function handleClick(evt) {
+    console.log(evt);
+    if (evt.target.id === 'winning-cell-2') {
+        boardTwo()
+    } else if (evt.target.className === 'cell2') {
+        wrongCell2()
+    }
+
+}
+function handleClick(evt) {
+    console.log(evt);
+    if (evt.target.id === 'winning-cell-3') {
+        boardThree()
+    } else if (evt.target.className === 'cell3') {
+        wrongCell3()
     }
 
 }
